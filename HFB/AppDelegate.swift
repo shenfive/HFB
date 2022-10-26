b9027367307533a2a8f06ad293dbe824e5e6dec4
Shen Jun Wu <shenfive@gmail.com>
2022-10-26 10:32:00 +0800

    Sample

--- HFB/AppDelegate.swift
+++ HFB/AppDelegate.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import Firebase
+import FacebookCore
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -13,9 +15,62 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        FirebaseApp.configure()
         // Override point for customization after application launch.
+        ApplicationDelegate.shared.application(
+            application,
+            didFinishLaunchingWithOptions: launchOptions
+        )
+        
+        
         return true
     }
+    
+    
+    
+//// AppDelegate.swift
+//import UIKit
+//import FacebookCore
+//
+//@UIApplicationMain
+//class AppDelegate: UIResponder, UIApplicationDelegate {
+//    func application(
+//        _ application: UIApplication,
+//        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+//    ) -> Bool {
+//        ApplicationDelegate.shared.application(
+//            application,
+//            didFinishLaunchingWithOptions: launchOptions
+//        )
+//
+//        return true
+//    }
+//
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:] ) -> Bool {
+        ApplicationDelegate.shared.application(
+            app,
+            open: url,
+            sourceApplication: options[UIApplication.OpenURLOptionsKey.sourceApplication] as? String,
+            annotation: options[UIApplication.OpenURLOptionsKey.annotation]
+        )
+    }
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
     // MARK: UISceneSession Lifecycle
 
--- HFB/ViewController.swift
+++ HFB/ViewController.swift
@@ -6,14 +6,84 @@
 //
 
 import UIKit
+import FacebookLogin
+import Firebase
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, LoginButtonDelegate {
 
+    let loginButton = FBLoginButton()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        loginButton.permissions = ["public_profile", "email"]
+        loginButton.center = view.center
+        loginButton.delegate = self
+        view.addSubview(loginButton)
+    
+        getFBuserData()
+        
+        Auth.auth().addStateDidChangeListener { auth, user in
+            if user == nil {
+                
+            }else{
+                
+            }
+        }
+        
     }
+    
+    
+    
+    func getFBuserData(){
+        if let token = AccessToken.current,
+            !token.isExpired {
+            // User is logged in, do work such as go to next view controller.
+            print("已登入 FB \(token.userID):\(token.permissions)")
+            GraphRequest(graphPath: "me", parameters: ["fields":"id, name, email"]).start { (connection, result, error) in
+                // check error
+                if error != nil {
+                    print("Failed to start graph request: \(error?.localizedDescription)")
+                    return
+                }
+                
+                if let userDict = result as? NSDictionary {
+                    let name = userDict["name"] as? String
+                    let id = userDict["id"] as? String
+                    let email = userDict["email"] as? String
+                    
+                    print(name as Any)
+                    print(id as Any)
+                    print(email as Any)
+                }
+            }
+        }else{
+            print("未登入 FB")
+        }
 
+    }
+    
+    
+    
 
+    //MARK: FBLogin Delegate
+    func loginButton(_ loginButton: FBLoginButton, didCompleteWith result: LoginManagerLoginResult?, error: Error?) {
+        getFBuserData()
+        if !(AccessToken.current?.isExpired ?? true) {
+            let credential = FacebookAuthProvider.credential(withAccessToken: AccessToken.current!.tokenString)
+            Auth.auth().signIn(with: credential)
+        }
+        
+        
+    }
+    
+    
+    func loginButtonDidLogOut(_ loginButton: FBLoginButton) {
+        getFBuserData()
+    }
+    
+    
+    
 }
 
